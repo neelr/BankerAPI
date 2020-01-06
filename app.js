f93686256f67f2aa7fca25c26e6b3942e0416931
Neel Redkar <neel.redkar@outlook.com>
2020-01-06 13:08:38 -0800

    add icon

--- app.js
+++ app.js
@@ -7,50 +7,88 @@ app.use(express.json());
 var Airtable = require('airtable');
 var base = new Airtable({ apiKey: process.env.AIRTABLE_KEY }).base(process.env.AIRTABLE_BASE);
 
-
-var send = (gp, id, reason, give_id) => {
+var slack = (channel,text) => {
+    axios.post('https://slack.com/api/chat.postMessage',
+        qs.stringify({
+            token: process.env.SLACK_TOKEN,
+            channel: channel,
+            text: text
+        })
+    );
+}
+var send = (gp, id, reason, bot_id) => {
     return new Promise((res, rej) => {
-        var giveUser;
-        base("bank").select({ view: "Grid view" })
+        var bot,user;
+        base("bank").select({ view: "Grid view" ,filterByFormula:"OR({User} = '"+bot_id+"', {User} = '"+id+"')"})
             .eachPage((records, fetch) => {
                 records.forEach((record) => {
-                    if (give_id == record.get("User")) {
-                        giveUser = { id: record.id, gp: record.get("Balance") };
+                    console.log(record.get("User"))
+                    if (bot_id == record.get("User")) {
+                        bot = { id: record.id, gp: record.get("Balance") };
+                    } else if (id == record.get("User")) {
+                        user = { id: record.id, gp: record.get("Balance") };
                     }
-                })
+     
+                },(err) => console.log(err))
                 fetch();
             }, () => {
-                if ((giveUser.gp - gp) >= 0) {
+                if ((bot.gp - gp) >= 0) {
                     res()
-                    axios.post('https://slack.com/api/chat.postMessage',
-                        qs.stringify({
-                            token: process.env.SLACK_TOKEN,
-                            channel: "UH50T81A6",
-                            text: `<@UH50T81A6> give <@${id}> ${gp}gp for ${reason}`,
-                            as_user: true
-                        })
-                    );
                     base('bank').update([
                         {
-                            "id": giveUser.id,
+                            "id": bot.id,
                             "fields": {
-                                "Balance": giveUser.gp - gp
+                                "Balance": bot.gp - gp
+                            }
+                        },
+                        {
+                            "id": user.id,
+                            "fields": {
+                                "Balance": user.gp + gp
                             }
                         }
                     ])
+                    slack(id,`<@${bot_id}> gave you ${gp}gp for the reason - "${reason}", and now you have ${user.gp+gp}gp`);
                 } else {
                     rej()
                 }
             })
     })
 }
-var auth = (uuid, token) => {
+var fine = (gp, id, reason, bot_id) => {
+    return new Promise((res, rej) => {
+        var bot,user;
+        base("bank").select({ view: "Grid view" ,filterByFormula:"{User} = '"+id+"'"})
+            .eachPage((records, fetch) => {
+                records.forEach((record) => {
+                    user = { id: record.id, gp: record.get("Balance") };
+                },(err) => console.log(err))
+                fetch();
+            }, () => {
+                if ((user.gp - gp) >= 0) {
+                    res()
+                    base('bank').update([
+                        {
+                            "id": user.id,
+                            "fields": {
+                                "Balance": user.gp - gp
+                            }
+                        }
+                    ])
+                    slack(id,`<@${bot_id}> fined you ${gp}gp for the reason - "${reason}", and now you have ${user.gp -gp}gp`);
+                } else {
+                    rej()
+                }
+            })
+    })
+}
+var auth = (uuid, token, type) => {
     return new Promise((res, rej) => {
         base('api').select({
             view: "Grid view"
         }).eachPage((records, fetchNextPage) => {
             records.forEach((record) => {
-                if (record.get("ID") == uuid && record.get("Token") == token) {
+                if (record.get("ID") == uuid && record.get("Token") == token && record.get("Permissions").includes(type)) {
                     res();
                     return;
                 }
@@ -66,13 +104,27 @@ app.get("/", (req, res) => {
 })
 
 app.post("/give", (req, res) => {
-    var { give_id, send_id, gp, token, reason } = req.body;
+    var { bot_id, send_id, gp, token, reason } = req.body;
     reason = reason ? reason : "No reason given";
-    auth(give_id, token)
+    auth(bot_id, token,"Give")
         .then(() => {
-            send(gp, send_id, reason, give_id)
+            send(gp, send_id, reason, bot_id)
                 .then(() => res.sendStatus(200))
                 .catch(() => res.send("Insufficient Balance"))
+        })
+        .catch((err) => {
+            console.log(err)
+            res.sendStatus(401);
+        })
+})
+app.post("/fine", (req, res) => {
+    var { bot_id, send_id, gp, token, reason } = req.body;
+    reason = reason ? reason : "No reason given";
+    auth(bot_id, token,"Fine")
+        .then(() => {
+            fine(gp, send_id, reason, bot_id)
+                .then(() => res.sendStatus(200))
+                .catch(() => res.send("Error, Contact @neelr"))
         })
         .catch((err) => {
             console.log(err)
